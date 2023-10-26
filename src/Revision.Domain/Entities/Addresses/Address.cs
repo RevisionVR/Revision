683f42e18d168919f68e66d241b3c90bcb3b2f42
Nurullo <ghostone234qwer@gmail.com>
2023-10-26 18:11:21 +0500

    Added address

---
+++ src/Revision.Domain/Entities/Addresses/Address.cs
@@ -0,0 +1,20 @@
+﻿using Revision.Domain.Commons;
+
+namespace Revision.Domain.Entities.Addresses;
+
+public class Address : Auditable
+{
+    public string Home { get; set; } = string.Empty;
+    public int Description { get; set; }
+    public float Longitude { get; set; }
+    public float Latitude { get; set; }
+
+    public long CountryId { get; set; }
+    public Country Country { get; set; }
+
+    public long RegionId { get; set; }
+    public Region Region { get; set; }
+
+    public long DistrictId { get; set; }
+    public District District { get; set; }
+}
---
+++ src/Revision.Domain/Entities/Addresses/Country.cs
@@ -0,0 +1,9 @@
+﻿using Revision.Domain.Commons;
+
+namespace Revision.Domain.Entities.Addresses;
+
+public class Country : Auditable
+{
+    public string Name { get; set; } = string.Empty;
+    public string CountryCode { get; set; } = string.Empty;
+}
