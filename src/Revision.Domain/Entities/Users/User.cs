f0158fa1e2487111546fe6399ab12a38507b53be
rustambekovich <muhammadqodir5050@gmail.com>
2023-10-26 18:18:10 +0500

    Created Education Entity

--- src/Revision.Domain/Entities/Users/User.cs
+++ src/Revision.Domain/Entities/Users/User.cs
@@ -9,7 +9,7 @@ public class User : Auditable
     public string LastName { get; set; } = string.Empty;
     public string MiddleName {  get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public string Salt { get; set; } = string.Empty;
     // adress
