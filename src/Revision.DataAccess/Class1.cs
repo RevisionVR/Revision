949919d7927266ad1fdb6bb02b2b2589ae11159f
Nurullo <ghostone234qwer@gmail.com>
2023-10-26 17:21:24 +0500

    Added repository

--- src/Revision.DataAccess/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Revision.DataAccess
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Revision.DataAccess/IRepositories/IRepository.cs
@@ -0,0 +1,6 @@
+﻿
+namespace Revision.DataAccess.IRepositories;
+
+public interface IRepository<TEntity> where TEntity 
+{
+}
