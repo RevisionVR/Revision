16b669d671b98b58302ec29621f3f6c626aeae53
rustambekovich <muhammadqodir5050@gmail.com>
2023-10-26 18:51:45 +0500

    DOMAIN : Created Device Payment

---
+++ src/Revision.Domain/Entities/Payments/DevicePayment.cs
@@ -0,0 +1,16 @@
+﻿using Revision.Domain.Commons;
+using Revision.Domain.Entities.Educations;
+
+namespace Revision.Domain.Entities.Payments;
+
+public class DevicePayment : Auditable
+{
+    public decimal Price { get; set; } 
+    public decimal TotalPrice { get; set; }
+    public int DeviceCount { get; set; }
+    public DateTime LastDay { get; set; }
+    public DateTime NextDay { get; set; }
+
+    public long EducationId { get; set; }
+    public Education Education { get; set; }
+}
