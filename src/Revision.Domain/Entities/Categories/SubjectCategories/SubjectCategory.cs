690837a95ae63c56b0a97d58b12a0608f5df5241
Nurullo <ghostone234qwer@gmail.com>
2023-10-26 18:27:01 +0500

    Domain : Added subject categories

---
+++ src/Revision.Domain/Entities/Categories/SubjectCategories/SubjectCategory.cs
@@ -0,0 +1,8 @@
+﻿using Revision.Domain.Commons;
+
+namespace Revision.Domain.Entities.Categories.SubjectCategories;
+
+public class SubjectCategory : Auditable
+{
+    public string Name { get; set; } = string.Empty;
+}
