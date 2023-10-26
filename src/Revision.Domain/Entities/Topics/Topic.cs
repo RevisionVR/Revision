484312f1a451a4b36b2a169d6158dc8966636a47
Nurullo <ghostone234qwer@gmail.com>
2023-10-26 18:48:46 +0500

    DOMAIN : Fixed errors and write topic payment

--- src/Revision.Domain/Entities/Topics/Topic.cs
+++ src/Revision.Domain/Entities/Topics/Topic.cs
@@ -5,7 +5,7 @@ namespace Revision.Domain.Entities.Topics;
 
 public class Topic : Auditable
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
 
     public long SubjectId { get; set; }
