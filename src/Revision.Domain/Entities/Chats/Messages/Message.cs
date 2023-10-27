283044ebab2f1fb16897e969c5da2913c04dea96
Nurullo <ghostone234qwer@gmail.com>
2023-10-27 12:00:28 +0500

    DOMAIN : Added relations with entities

--- src/Revision.Domain/Entities/Chats/Messages/Message.cs
+++ src/Revision.Domain/Entities/Chats/Messages/Message.cs
@@ -1,17 +1,12 @@
 ﻿using Revision.Domain.Commons;
-using Revision.Domain.Entities.Chats.Conversations;
-using Revision.Domain.Entities.Educations;
+using Revision.Domain.Entities.Chats.Rooms;
 
 namespace Revision.Domain.Entities.Chats.Messages;
 
 public class Message : Auditable
 {
-    public string MessageText { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
 
-    public long ConversationID { get; set; }
-    public Conversation Conversation { get; set; }
-
-    public long SenderID { get; set; }
-    public Education Education { get; set; }
-
+    public long RoomId { get; set; }
+    public Room Room { get; set; }
 }
---
+++ src/Revision.Domain/Entities/Chats/Rooms/EducationRoom.cs
@@ -0,0 +1,10 @@
+﻿using Revision.Domain.Commons;
+using Revision.Domain.Entities.Educations;
+
+namespace Revision.Domain.Entities.Chats.Rooms;
+
+public class EducationRoom : Auditable
+{
+    public long EducationId { get; set; }
+    public Education Education { get; set; }
+}
