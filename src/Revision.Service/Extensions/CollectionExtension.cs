c8d824f914ad7242b0f417f0b3772b4593d470db
Nurullo <ghostone234qwer@gmail.com>
2023-10-27 16:48:50 +0500

    SERVICE : Added collection extension

---
+++ src/Revision.Service/Extensions/CollectionExtension.cs
@@ -0,0 +1,9 @@
+﻿using Revision.Domain.Configurations;
+
+namespace Revision.Service.Extensions;
+
+public static class CollectionExtension
+{
+    public static IQueryable<T> ToPaginate<T>(this IQueryable<T> values, PaginationParams pagination)
+        => values.Skip((pagination.PageIndex - 1) * pagination.PageSize).Take(pagination.PageSize);
+}
