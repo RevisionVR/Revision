92d601901888683351c0a508eda3c9c713c50e0b
rustambekovich <muhammadqodir5050@gmail.com>
2023-10-28 18:29:08 +0500

    User Createad Validation

---
+++ src/Revision.Service/Validations/EmailValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace Revision.Service.Validations;
+
+public class EmailValidation
+{
+    public static bool IsValid(string email)
+    {
+        if (email.Length < 0 && email.Contains("@") && email.Contains("."))
+        {
+            return false;
+        }
+        return true;
+    }
+}
