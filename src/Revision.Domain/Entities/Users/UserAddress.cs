d9c33a4308017ea4d72299de5b06249993593466
Nurullo <ghostone234qwer@gmail.com>
2023-10-28 11:52:25 +0500

    SERVICE : Changes

---
+++ src/Revision.Domain/Entities/Users/UserAddress.cs
@@ -0,0 +1,13 @@
+﻿using Revision.Domain.Commons;
+using Revision.Domain.Entities.Addresses;
+
+namespace Revision.Domain.Entities.Users;
+
+public class UserAddress : Auditable
+{
+    public long UserId { get; set; }
+    public User User { get; set; }
+
+    public long AddressId { get; set; }
+    public Address Address { get; set; }
+}
