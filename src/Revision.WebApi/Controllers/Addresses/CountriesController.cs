7f0d87d8193dd7b1b9b9da172d6a40af836b4812
Nurullo <ghostone234qwer@gmail.com>
2023-10-30 19:12:31 +0500

    API : Added county , district, region controllers

---
+++ src/Revision.WebApi/Controllers/Addresses/CountriesController.cs
@@ -0,0 +1,43 @@
+﻿using Revision.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Revision.Service.Interfaces.Addresses;
+
+namespace Revision.WebApi.Controllers.Addresses;
+
+public class CountriesController : BaseController
+{
+    private readonly ICountryService _countryService;
+    public CountriesController(ICountryService countryService)
+    {
+        _countryService = countryService;
+    }
+
+    [HttpPost("set")]
+    public async Task<IActionResult> PostAsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _countryService.SetAsync()
+        });
+
+
+    [HttpGet("get/{id:long}")]
+    public async Task<IActionResult> GetByIdAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _countryService.GetByIdAsync(id)
+        });
+
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllAsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _countryService.GetAllAsync()
+        });
+}
---
+++ src/Revision.WebApi/Controllers/Addresses/DistrictsController.cs
@@ -0,0 +1,43 @@
+﻿using Revision.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Revision.Service.Interfaces.Addresses;
+
+namespace Revision.WebApi.Controllers.Addresses;
+
+public class DistrictsController : BaseController
+{
+    private readonly IDistrictService _districtService;
+    public DistrictsController(IDistrictService districtService)
+    {
+        _districtService = districtService;
+    }
+
+    [HttpPost("set")]
+    public async Task<IActionResult> PostAsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _districtService.SetAsync()
+        });
+
+
+    [HttpGet("get/{id:long}")]
+    public async Task<IActionResult> GetByIdAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _districtService.GetByIdAsync(id)
+        });
+
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllAsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _districtService.GetAllAsync()
+        });
+}
