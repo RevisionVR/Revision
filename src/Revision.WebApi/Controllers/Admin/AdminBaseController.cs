22f55852376d25434f26f1c0dc1060e4aad685b8
rustambekovich <muhammadqodir5050@gmail.com>
2023-10-31 12:12:08 +0500

    WEBAPI : Controller Architecture

---
+++ src/Revision.WebApi/Controllers/Admin/AdminBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Revision.WebApi.Controllers.Admin
+{
+    [Authorize(Roles = "Admin, SuperAdmin")]
+    public class AdminBaseController : ControllerBase
+    {}
+}
---
+++ src/Revision.WebApi/Controllers/Client/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Revision.WebApi.Controllers.Client
+{
+    [Authorize(Roles = "User")]
+    public class BaseController : ControllerBase
+    {}
+}
