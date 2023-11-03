8b92a0f4800491809af1101cc2fc31b043df0c66
Nurullo <ghostone234qwer@gmail.com>
2023-11-03 18:31:50 +0500

    API : Added user education cottroller deleted wep

---
+++ src/Revision.WebApi/Controllers/Common/Educations/CommonUserEducationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Revision.Service.Interfaces.Educations;
+using Revision.WebApi.Models;
+
+namespace Revision.WebApi.Controllers.Common.Educations;
+
+public class CommonUserEducationController : BaseController
+{
+    private readonly IUserEducationService _userEducationService;
+    public CommonUserEducationController(IUserEducationService userEducationService)
+    {
+        _userEducationService = userEducationService;
+    }
+
+    [HttpGet("get-by-education/{educationId:{long}")]
+    public async Task<IActionResult> GetByEducationIdAsync(long educationId)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userEducationService.GetByEducationIdAsync(educationId)
+        });
+}
