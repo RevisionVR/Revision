3d6678066e1c870693358fab3d7effb53b9e4729
Nurullo <ghostone234qwer@gmail.com>
2023-11-04 16:43:26 +0500

    SERVICE : user service

---
+++ src/Revision.WebApi/Controllers/Admin/Users/AdminUsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Revision.Domain.Configurations;
+using Revision.Service.Interfaces.Users;
+using Revision.WebApi.Models;
+
+namespace Revision.WebApi.Controllers.Admin.Users;
+
+public class AdminUsersController : AdminBaseController
+{
+    private readonly IUserService _userService;
+    public AdminUsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpDelete("delete/{id:long}")]
+    public async Task<IActionResult> DeleteAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.DeleteAsync(id)
+        });
+
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllAsync([FromBody] PaginationParams pagination)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.GetAllAsync(pagination)
+        });
+}
