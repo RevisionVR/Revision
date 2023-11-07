e401457bbf8218172e7525a79f17779db65ba8d1
rustambekovich <muhammadqodir5050@gmail.com>
2023-11-07 11:19:09 +0500

    WEBAPI : Automatic migration extation

---
+++ src/Revision.WebApi/Extensions/DataExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Revision.DataAccess.Contexts;
+
+namespace Revision.WebApi.Extensions;
+
+public static class DataExtensions
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<RevisionDbContext>();
+
+        db.Database.Migrate();
+    }
+}
--- src/Revision.WebApi/Program.cs
+++ src/Revision.WebApi/Program.cs
@@ -28,6 +28,8 @@ builder.Services.AddCustomServices();
 
 var app = builder.Build();
 
+app.ApplyMigrations();
+
 PathHelper.WebRootPath = Path.GetFullPath("wwwroot");
 EnvironmentHelper.CountryPath = Path.GetFullPath(builder.Configuration.GetValue<string>(("FilePath:CountriesFilePath")));
 EnvironmentHelper.RegionPath = Path.GetFullPath(builder.Configuration.GetValue<string>(("FilePath:RegionsFilePath")));
