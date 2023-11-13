1f4d829f6449f9666346983f36e76ffb982013cc
rustambekovich <muhammadqodir5050@gmail.com>
2023-11-13 18:21:12 +0500

    change fixsed

--- src/Revision.WebApi/Controllers/Admin/Users/AdminUsersController.cs
+++ src/Revision.WebApi/Controllers/Admin/Users/AdminUsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Revision.Domain.Configurations;
+using Revision.Service.DTOs.Users;
 using Revision.Service.Interfaces.Users;
+using Revision.Service.Validations.Users;
 using Revision.WebApi.Models;
 
 namespace Revision.WebApi.Controllers.Admin.Users;
@@ -40,4 +42,27 @@ public class AdminUsersController : AdminBaseController
             Message = "Success",
             Data = await _userService.GetAllAsync(pagination)
         });
+
+    [HttpPut("update/{id:long}")]
+    public async Task<IActionResult> UpdateAsync(long id, [FromForm] UserUpdateDto dto)
+    {
+        var validation = new UserUpdateDtoValidator();
+        var result = await validation.ValidateAsync(dto);
+
+        if (result.IsValid)
+        {
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Success",
+                Data = await _userService.UpdateAsync(id, dto)
+            });
+        }
+
+        return BadRequest(new Response
+        {
+            StatusCode = 400,
+            Message = result.Errors.FirstOrDefault().ToString()
+        });
+    }
 }
