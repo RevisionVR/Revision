c9ce2b82cf799b4d75a14ee2e4e29a6c1924da14
Nurullo <ghostone234qwer@gmail.com>
2023-11-13 17:47:50 +0500

    Open comment and change education service

--- src/Revision.Service/DTOs/Regions/RegionResultDto.cs
+++ src/Revision.Service/DTOs/Regions/RegionResultDto.cs
@@ -1,8 +1,10 @@
-﻿namespace Revision.Service.DTOs.Regions;
+﻿using Revision.Service.DTOs.Countries;
+
+namespace Revision.Service.DTOs.Regions;
 
 public class RegionResultDto
 {
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    // public CountryResultDto Country { get; set; }
+    public CountryResultDto Country { get; set; }
 }
